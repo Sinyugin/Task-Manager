f0b376cdb73bdaf4355d69cec8ffcdbe0f0f5a30
Sergey <elec84@mail.ru>
2024-02-18 15:07:25 +0300

    Create project

---
+++ src/test/java/ru/sinyugin/TaskManager/TaskManagerAppApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.sinyugin.TaskManager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskManagerAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
